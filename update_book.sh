3ee07af134096c1ec6e65d62d3e3db932d7ec4d3
Nanna Holmgaard List <nanna.h.list@gmail.com>
2023-08-08 21:12:56 +0200

    nhlist: update instructions

--- update_book.sh
+++ update_book.sh
@@ -1,2 +1,11 @@
+# To install Jupyter Book
+# conda install -c conda-forge jupyter-book
+
+# To install gh pages
 #https://jupyterbook.org/en/stable/publish/gh-pages.html
-#To update your online book, make changes to your book’s content on the main branch of your repository, re-build your book with jupyter-book build mybookname/ and then use ghp-import -n -p -f mylocalbook/_build/html as before to push the newly built HTML to the gh-pages branch.
+# conda install -c conda-forge ghp-import
+
+#To update your online book:
+#1) make changes to your book’s content on the main branch of your repository
+#2) re-build your book with jupyter-book build PythonExerciseJupyterBook 
+#3) then use ghp-import -n -p -f PythonExerciseJupyterBook/_build/html as before to push the newly built HTML to the gh-pages branch.
